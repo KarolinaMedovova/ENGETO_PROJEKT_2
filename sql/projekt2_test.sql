67dd35e0b58e1b94cac6ce78315525ac67943445
Karolina Medovova <student.karolina@seznam.cz>
2026-01-23 23:20:23 +0100

    Oprava dat.typu sloupce nazev z TEXT na VARCHAR(255).

--- sql/projekt2_test.sql
+++ sql/projekt2_test.sql
@@ -7,7 +7,7 @@ USE projekt2_test;
 -- Vytvoření tabulky, pokud neexistuje
 CREATE TABLE IF NOT EXISTS ukoly (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nazev TEXT NOT NULL,
+    nazev VARCHAR(255) NOT NULL,
     popis TEXT NOT NULL,
     stav VARCHAR(20) NOT NULL DEFAULT 'nezahájeno',
     datum_vytvoreni DATE NOT NULL DEFAULT (CURRENT_DATE)
